cdc6927f5bc4b138c3244030ac36009e204198aa
CountNeuroman <olorinoren@gmail.com>
2023-10-07 22:29:01 +0300

    Fix scripts

--- delete.sh
+++ delete.sh
@@ -6,7 +6,5 @@ sudo k3s kubectl delete deployment managed-node1
 sudo k3s ctr images delete docker.io/library/control-node:0.0.1 
 sudo k3s ctr images delete docker.io/library/managed-node1:0.0.1 
 
-# Work in fish shell
-# Bash need this format: docker rmi $(docker images -q "example_image")
-docker rmi (docker images -q "control-node")
-docker rmi (docker images -q "managed-node1")
+docker rmi $(docker images -q "control-node")
+docker rmi $(docker images -q "managed-node1")
